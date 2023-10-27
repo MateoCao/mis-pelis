863600d1408ef4c16a524de5256dfa9f346c537e
Mateo Cao <mateocao12@gmail.com>
2023-10-26 22:58:02 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App () {
   return (
     <>
       <h1>REACT</h1>
-      <h2>ESTO FUNCIONA</h2>
+      <h2>TESTING</h2>
       <Router>
         <Routes>
           <Route path='/login' element={<LoginPage />} />
