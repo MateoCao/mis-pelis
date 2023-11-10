7552bcb5e9a19a6366a9cbc5344757527a5f8496
Mateo Cao <mateocao12@gmail.com>
2023-11-09 22:43:14 -0300

    feature/context

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
+import { appRouter } from './core/rutes/app_router';
+import { MoviesProvider } from './context/MoviesContext';
+import Navbar from './components/Navbar';
 
 function App () {
   return (
     <>
-      <h1>REACT</h1>
-      <h2>DEV</h2>
-      <Router>
-        <Routes>
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/register' element={<RegisterPage />} />
-        </Routes>
-      </Router>
+      <MoviesProvider>
+        <Navbar />
+        <RouterProvider router={appRouter} />
+      </MoviesProvider>
     </>
   );
 }
